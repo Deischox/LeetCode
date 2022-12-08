1f7f85b9a8ebf90890b55526ee3e6e7f49884785
Silasueber <51875946+Deischox@users.noreply.github.com>
2022-12-08 13:13:34 +0100

    Time: 100 ms (80.26%), Space: 43.4 MB (20.43%) - LeetHub

---
+++ 1-two-sum/1-two-sum.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var map = {} 
+    for(var i = 0; i < nums.length; i++)
+    {
+        if(nums[i] in map)return [i,map[nums[i]]]
+        else map[target-nums[i]] = i
+    }
+};
